168cd8efb18adef8e89cfa132ff862f7de624d5f
TuesdayHu <tuesday_hu@hotmail.com>
2023-02-26 09:41:35 +0800

    First push

---
+++ TransparencyPrinter/TransparencyPrinterCommand.cs
@@ -0,0 +1,32 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System;
+using System.Collections.Generic;
+
+namespace TransparencyPrinter
+{
+    public class TransparencyPrinterCommand : Command
+    {
+        public TransparencyPrinterCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static TransparencyPrinterCommand Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "TransparencyPrinter";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // Usually commands in export plug-ins are used to modify settings and behavior.
+            // The export work itself is performed by the TransparencyPrinterPlugin class.
+            return Result.Success;
+        }
+    }
+}
